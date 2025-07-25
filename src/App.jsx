7d510527d70c0a906a8cabdcb0d0a816e474c6f7
Daniel Owusu <owusudaniel077@gmail.com>
2025-07-25 12:35:56 +0000

    Import home edited

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Details from "./pages/details";
 import Favorites from "./pages/Favorites";
 import RootLayout from "./Layout/RootLayout";
