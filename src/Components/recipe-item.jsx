5c172b2585ba6ed22771dbbfcc9d73b4b7dc5ffa
Daniel Owusu <owusudaniel077@gmail.com>
2025-07-25 13:13:57 +0000

    Import Home edited in recipe-item.jsx

--- src/Components/recipe-item.jsx
+++ src/Components/recipe-item.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Home from "../pages/home";
+import Home from "../pages/Home";
 import { Link } from "react-router-dom";
 import { useOutletContext } from "react-router-dom";
 
